c0d6f276a4913176dffae12565edf078ff34865c
yousef-201300829 <ya1300829@qu.edu.qa>
2016-05-14 19:09:15 +0300

    member

--- entities/staff.js
+++ entities/staff.js
@@ -1,3 +1,18 @@
 /**
  * Created by Ahmed on 5/13/2016.
  */
+class staff{
+    
+    constructor(firstName,lastName,mobile,email,username,password,staffId)
+    {
+        super(firstName,lastName,mobile,email,username,password)
+        this.staffId = staffId;
+    }
+    
+    getStaffId()
+    {
+        return this.staffId;
+    }
+    
+}
+module.exports = staff;
