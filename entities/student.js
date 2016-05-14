c882c876489c5b2538abd1c95278d737e26ad913
yousef-201300829 <ya1300829@qu.edu.qa>
2016-05-14 19:15:33 +0300

    student

--- entities/student.js
+++ entities/student.js
@@ -1,3 +1,19 @@
 /**
  * Created by Ahmed on 5/13/2016.
  */
+
+class Student{
+
+    constructor(firstName,lastName,mobile,email,username,password,memeberId)
+    {
+        super(firstName,lastName,mobile,email,username,password)
+        this.memberId = memeberId;
+    }
+
+    getStudentId()
+    {
+        return this.memberId;
+    }
+}
+
+module.exports = Student;
