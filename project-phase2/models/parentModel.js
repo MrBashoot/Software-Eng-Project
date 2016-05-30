832e1820627459453ae1ca35b86425106670f549
Ahmed Ibrahim <tnterminate@hotmail.com>
2016-05-30 21:55:21 +0300

    parent

---
+++ project-phase2/models/parentModel.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ahmed on 5/30/2016.
+ */
+
+'use strict';
+let mongoose = require('mongoose');
+
+let parentSchema = new mongoose.Schema({
+    qatariId: Number,
+    firstName: String,
+    lastName: String,
+    mobile: String,
+    email: String,
+    username: String,
+    password: String,
+    students: []
+});
+
+module.exports = mongoose.model('teacher', parentSchema);
