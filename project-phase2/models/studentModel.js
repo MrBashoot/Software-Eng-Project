45fae352672079f7e6f8c320b2c843dbf4f46843
ahmed-201204104 <jnd-35@hotmail.com>
2016-05-30 21:58:20 +0300

    student

---
+++ project-phase2/models/studentModel.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Ahmed on 5/30/2016.
+ */
+'use strict';
+let mongoose = require('mongoose');
+
+let studentSchema = new mongoose.Schema({
+    studentID: Number,
+    firstname: String,
+    lastname: String,
+    dob: String,
+    gender: String,
+    schoolgrade: number,
+    teacherID: number
+});
+
+module.exports = mongoose.model('student', studentSchema);
