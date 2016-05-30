2553bfb4dd503581538cc900ff446961c6ac3135
Ahmed Ibrahim <tnterminate@hotmail.com>
2016-05-30 21:50:17 +0300

    commit 2

---
+++ project-phase2/models/teacherModel.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Ahmed on 5/30/2016.
+ */
+'use strict';
+let mongoose = require('mongoose');
+
+let teacherSchema = new mongoose.Schema({
+    staffNo: Number,
+    username: String,
+    firstName: String,
+    lastName: String,
+    email: String,
+    password: String,
+    isCoordinator: Boolean
+});
+
+module.exports = mongoose.model('teacher', teacherSchema);
