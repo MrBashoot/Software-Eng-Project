f07a5e998b359203d9169e240b96967e7722d395
ahmed-201204104 <jnd-35@hotmail.com>
2016-06-03 21:57:58 +0300

    task

---
+++ project-phase2/models/taskModel.js
@@ -0,0 +1,24 @@
+
+
+'use strict';
+
+let mongoose = require('mongoose');
+
+let taskSchema = new mongoose.Schema({
+    taskId: Number,
+    studentID: Number,
+    surahId: Number,
+    surahName: String,
+    fromAya: Number,
+    toAya: Number,
+    type: String,
+    dueDate: Date,
+    completedDate: Date,
+    masteryLevel: String,
+    comment: String
+});
+
+module.exports = mongoose.model('task', taskSchema);
+
+
+//comment this
